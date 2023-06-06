89f21798150fd1d95e7ba7764cb77552b79208dd
Aynug <aynug21@gmail.com>
2023-06-06 12:43:35 -0700

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for k in range(ord('a'), ord('z') + 1):
-    if k != ord('e') and k != ord('q'):
-        print("{:k}".format(k), end="")
+for k in range(97, 123):
+    if chr(k) is not 'q' and chr(k) is not 'e':
+        print("{}".format(chr(k)), end="")
