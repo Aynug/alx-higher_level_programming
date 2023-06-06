c58718910490ffb543ca01e2be187599c2e1a88b
Aynug <aynug21@gmail.com>
2023-06-06 13:07:09 -0700

    updated

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for k in range(97, 123):
-    if chr(k) is not 'q' and chr(k) is not 'e':
-        print("{}".format(chr(k)), end="")
+    if k != 101 and k != 113:
+        print('{}'.format(chr(k)), end='')
